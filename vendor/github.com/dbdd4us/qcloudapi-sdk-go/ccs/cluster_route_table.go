1efeb747305692ba911a5f6c89b57bbc38eb12fa
yundawang <yundawang@tencent.com>
2018-05-24 14:43:22 +0800

    update vendor

--- vendor/github.com/dbdd4us/qcloudapi-sdk-go/ccs/cluster_route_table.go
+++ vendor/github.com/dbdd4us/qcloudapi-sdk-go/ccs/cluster_route_table.go
@@ -18,6 +18,23 @@ type DeleteClusterRouteTableResponse struct {
 	Response
 }
 
+type DescribeClusterRouteTableArgs struct {
+}
+
+type DescribeClusterRouteTableResponse struct {
+	Response
+	Data struct {
+		TotalCount    int              `json:"TotalCount"`
+		RouteTableSet []RouteTableInfo `json:"RouteTableSet"`
+	} `json:"data"`
+}
+
+type RouteTableInfo struct {
+	RouteTableName      string `json:"RouteTableName"`
+	RouteTableCidrBlock string `json:"RouteTableCidrBlock"`
+	VpcId               string `json:"VpcId"`
+}
+
 func (client *Client) CreateClusterRouteTable(args *CreateClusterRouteTableArgs) (*CreateClusterRouteTableResponse, error) {
 	response := &CreateClusterRouteTableResponse{}
 	err := client.Invoke("CreateClusterRouteTable", args, response)
@@ -32,6 +49,15 @@ func (client *Client) DeleteClusterRouteTable(args *DeleteClusterRouteTableArgs)
 	err := client.Invoke("DeleteClusterRouteTable", args, response)
 	if err != nil {
 		return &DeleteClusterRouteTableResponse{}, err
+	}
+	return response, nil
+}
+
+func (client *Client) DescribeClusterRouteTable(args *DescribeClusterRouteTableArgs) (*DescribeClusterRouteTableResponse, error) {
+	response := &DescribeClusterRouteTableResponse{}
+	err := client.Invoke("DescribeClusterRouteTable", args, response)
+	if err != nil {
+		return &DescribeClusterRouteTableResponse{}, err
 	}
 	return response, nil
 }
